fc055daf8b683c28099671aba69db61d1b0aa507
sagepages <62083371+sagepages@users.noreply.github.com>
2021-10-13 22:11:40 -0400

    Add Login page

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,3 +2,4 @@ export { default as Navigation } from "./Navigation";
 export { default as Home } from "./Home";
 export { default as Events } from "./Events";
 export { default as Profile } from "./Profile";
+export { default as Login } from "./Login";
