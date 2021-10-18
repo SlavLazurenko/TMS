c36146e5daf57891517d77537a333c3b2a35d902
Slav Lazurenko <vlazurenko22@gmail.com>
2021-10-18 13:27:34 -0400

    Added header section
    header section includes : event search bar and create event button. No functionality, only style

--- frontend/src/components/Events.jsx
+++ frontend/src/components/Events.jsx
@@ -1,10 +1,40 @@
 import React from "react";
+import "../css/Events.css";
+
+function Search() {
+  return (
+    <form action="/events" method="get">
+      <label htmlFor="header-search">
+        <span className="visually-hidden">Search blog posts</span>
+      </label>
+      <input
+        type="text"
+        id="header-search"
+        placeholder="Search events"
+        name="s"
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+}
+
+function Create() {
+  return (
+    <button className="create-event">Create Event +</button>
+  );
+}
 
 function Events() {
 
   return (
     <div className="events">
-        <h3>Events</h3>
+      <div className="header">
+        <Search />
+        <Create />
+      </div>
+      <ol>
+
+      </ol>
     </div>
   );
 }
