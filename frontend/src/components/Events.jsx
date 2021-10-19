a6b37e113de3b61a62c07be7461400d95009f383
Slav Lazurenko <vlazurenko22@gmail.com>
2021-10-19 01:33:46 -0400

    Added basic event entries

--- frontend/src/components/Events.jsx
+++ frontend/src/components/Events.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "../css/Events.css";
+import eventDateIcon from "../img/event-date.svg";
+import participantsIcon from "../img/participants-count.svg";
+import durationIcon from "../img/duration.svg";
+import disciplineIcon from "../img/discipline.svg";
 
 function Search() {
   return (
@@ -24,6 +28,65 @@ function Create() {
   );
 }
 
+function ParticipantsInfo(props) {
+  let quantifier = props.count; 
+  let qualifier = "";
+
+  if (props.type === "player")
+    qualifier = "players";
+  else if (props.type === "team")
+    qualifier = "teams";
+  return (<span>{quantifier} {qualifier}</span>);
+}
+
+function DurationInfo(props) {
+  let [quantifier, qualifier] = ["", ""];
+
+  if (props.value / 1000 / 60 / 60 / 24 >= 1)
+    [quantifier, qualifier] = [(props.value / 1000 / 60 / 60 / 24).toString(), "days"];
+  else if (props.value / 1000 / 60 / 60 >= 1)
+    [quantifier, qualifier] = [(props.value / 1000 / 60 / 60).toString(), "hours"];
+  else if (props.value / 1000 / 60 >= 1)
+    [quantifier, qualifier] = [(props.value / 1000 / 60).toString(), "minutes"];
+  else
+    [quantifier, qualifier] = [(props.value / 1000).toString(), "seconds"];
+  return (<span>{Math.floor(quantifier)} {qualifier}</span>);
+}
+
+function EventEntry(props) {
+
+  const { data } = props;
+
+  return (
+    <div className="event" id={data.id}>
+      <h3 className="name">{data.name}</h3>
+      <ul className="info">
+        <li className="time">
+          <img src={eventDateIcon} alt="event date"></img>
+          <span>{new Intl.DateTimeFormat('en-US', { dateStyle: 'short', timeStyle: 'short' }).format(data.start)}</span>
+        </li>
+        <li className="participants">
+          <img src={participantsIcon} alt="maximum participants"></img>
+          <ParticipantsInfo
+            type={data.participantType}
+            count={data.maxParticipants}
+          />
+        </li>
+        <li className="duration">
+          <img src={durationIcon} alt="duration"></img>
+          <DurationInfo
+            value={data.end - data.start}
+          />
+        </li>
+        <li className="discipline">
+          <img src={disciplineIcon} alt="discipline"></img>
+          <span>{data.discipline}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
 const tmpEvents = [
   {
     id: 1,
@@ -36,7 +99,7 @@ const tmpEvents = [
     discipline: "DOTA 2"
   },
   {
-    id: 1,
+    id: 2,
     name: "Event #2",
     start: new Date(2021, 9, 24, 14),
     end: new Date(2021, 9, 24, 16),
@@ -46,7 +109,7 @@ const tmpEvents = [
     discipline: "World of Tanks"
   },
   {
-    id: 1,
+    id: 3,
     name: "Event #3",
     start: new Date(2021, 10, 5, 17),
     end: new Date(2021, 10, 12, 17),
@@ -56,7 +119,7 @@ const tmpEvents = [
     discipline: "Soccer"
   },
   {
-    id: 1,
+    id: 4,
     name: "Event #4",
     start: new Date(2021, 10, 12, 10),
     end: new Date(2021, 10, 12, 18),
@@ -77,11 +140,14 @@ function Events() {
         <Search />
         <Create />
       </div>
-      <ol>
+      <div className="entries">
         {events.map(event => (
-          <li>{event.name}</li>
+          <EventEntry 
+            key={event.id}
+            data={event}
+          />
         ))}
-      </ol>
+      </div>
     </div>
   );
 }
