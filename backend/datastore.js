f518654608cc7cd6a199ad28026cdb467dd74517
Slav Lazurenko <vlazurenko22@gmail.com>
2021-10-29 22:03:30 -0400

    Refactored Datastore module, added user object

--- backend/datastore.js
+++ backend/datastore.js
@@ -3,6 +3,101 @@ const { MongoClient } = require('mongodb');
 
 //IMPLEMENT ASYNC FUNCTIONS TO CALL MONGO (CONNECT, FIND)
 
+let client = null;
+
+isArray = function(a) {
+  return (!!a) && (a.constructor === Array);
+};
+
+isObject = function(a) {
+  return (!!a) && (a.constructor === Object);
+};
+
+isEmptyObject = function(obj) {
+  var name;
+  for (name in obj) {
+    return false;
+  }
+  return true;
+};
+
+const init = async() => {
+  try {
+    client = await MongoClient.connect(process.env.MONGODB_URI);
+    console.log("MongoDB connection successful");
+    return true;
+  } catch (e) {
+    console.log("MongoDB connection failed");
+    return false;
+  }
+}
+
+const get = async() => {
+  try {
+    if (client != null || await init()) return client;
+
+    throw new Error('Error connecting to db');
+
+  } catch (e) {
+    console.log("get error", e);
+    return false;
+  }
+}
+
+const user = {
+
+  add: async(...docs) => {
+    try {
+
+      const result = await (await get()).db('tms').collection('users').insertMany(docs, {ordered: false});
+      return result;
+
+    } catch (e) {
+      console.log("user add error", e);
+      return false;
+    }
+  },
+  update: async(filter, data) => {
+    try {
+      const result = await (await get()).db('tms').collection('users').updateMany(filter, {$set: data});
+      return result;
+
+    } catch (e) {
+      console.log("user update error", e);
+      return false;
+    }
+  },
+  find: async(filter) => {
+    try {
+      const result = await (await get()).db('tms').collection('users').find(filter).toArray();
+      return result;
+
+    } catch (e) {
+      console.log("user find error", e);
+      return false;
+    }
+
+  },
+  delete: async(filter) => {
+    try {
+      if(isEmptyObject(filter)) {
+        throw new Error('Argument for user.delete() cannot be empty');
+      }
+
+      const result = await (await get()).db('tms').collection('users').deleteOne(filter);
+      return result;
+
+    } catch (e) {
+      console.log("user find error", e);
+      return false;
+    }
+
+  }
+}
+
+module.exports = { get: get, user: user }
+
+/*
 class Datastore {
   constructor() {
     this.client = new MongoClient(process.env.MONGODB_URI);
@@ -22,6 +117,7 @@ class Datastore {
 
   }
 }
+*/
 
 
 
---
+++ backend/dbtest.js
@@ -0,0 +1,30 @@
+const Datastore = require("./datastore.js");
+
+async function main() {
+  
+  let result = await Datastore.user.find({});
+  // let result = await Datastore.user.delete({tag: "NewUser#4"});
+  // let result = await Datastore.user.update({tag: "Eventski596"}, {email: 'theeventski@ghotmail.com'});
+  // let result = await Datastore.user.add(
+  //   {
+  //     tag: "NewUser#2", 
+  //     email: "second@email.com", 
+  //     discordTag: "second"
+  //   },
+  //   {
+  //     tag: "NewUser#3", 
+  //     email: "third@email.com", 
+  //     discordTag: "none"
+  //   },
+  //   {
+  //     tag: "NewUser#4", 
+  //     email: "fourth@email.com", 
+  //     discordTag: "fourth"
+  //   }
+  // );
+
+  console.log(result);
+}
+
+
+main();
