ae48064b4e42b89f7a2649517392413e91d8bed1
Slav Lazurenko <vlazurenko22@gmail.com>
2021-10-31 00:44:26 -0400

    Completed UserDAO (update, find, remove)

--- backend/datastore/UserDAO.js
+++ backend/datastore/UserDAO.js
@@ -1,3 +1,11 @@
+isEmptyObject = function(obj) {
+  var name;
+  for (name in obj) {
+    return false;
+  }
+  return true;
+};
+
 class UserDao {
   constructor(db) {
     this.collection = db.collection("users");
@@ -7,13 +15,17 @@ class UserDao {
     try {
 
       //VALIDATE INPUT
-      docs = docs.filter((doc) => {
+      docs = docs.filter(doc => {
         return "tag" in doc && "email" in doc && "discordTag" in doc;
       })
-      .map((doc) => {
+      .map(doc => {
         const { tag, email, discordTag } = doc;
         return {tag, email, discordTag};
       });
+
+      if (isEmptyObject(docs)) {
+        throw new Error("Invalid agrument for User.add()");
+      }
 
       //EXECUTE
       const result = await this.collection.insertMany(docs, {ordered: false});
@@ -21,20 +33,67 @@ class UserDao {
       return { count: result.insertedCount, ids: result.insertedIds };
 
     } catch (e) {
-      const { nInserted, insertedIds } = e.result;
-      const failedIds = e.writeErrors.map((val) => { return val.err.op._id });
 
-      //FILTER IDS THAT WERE SUCCESSFULLY INSERTED
-      const actualInsertedIds = Object.keys(insertedIds)
-        .filter(function(key){
-          return !failedIds.includes(insertedIds[key]);
-        })
-        .reduce(function (result, key){
-          result[key] = insertedIds[key];
-          return result;
-        }, {});
+      if (e.result) {
+        const { nInserted, insertedIds } = e.result;
+        const failedIds = e.writeErrors.map((val) => { return val.err.op._id });
+
+        //FILTER IDS THAT WERE SUCCESSFULLY INSERTED
+        const actualInsertedIds = Object.keys(insertedIds)
+          .filter(key => {
+            return !failedIds.includes(insertedIds[key]);
+          })
+          .reduce((result, key) => {
+            result[key] = insertedIds[key];
+            return result;
+          }, {});
+        
+        return { error: e, count: nInserted, ids: actualInsertedIds };
+      }
+
+      return { error: e };
+    }
+  }
+
+  async update(selector, data) {
+    try {
+
+      data = Object.keys(data)
+      .filter(key => {
+        return key == "tag" || key == "discordTag" || key == "email";
+      })
+      .reduce((result, key) => {
+        result[key] = data[key];
+        return result;
+      }, {});
+
+      const result = await this.collection.updateMany(selector, {$set: data});
+
+      return { count: result.modifiedCount };
       
-      return { error: e, count: nInserted, ids: actualInsertedIds };
+    } catch (e) {
+      return { error: e };
+    }
+  }
+
+  async find(selector) {
+    try {
+      const result = await this.collection.findOne(selector);
+      return result;
+    } catch (e) {
+      return { error: e };
+    }
+  }
+
+  async remove(selector) {
+    try {
+      if(isEmptyObject(selector)) {
+        throw new Error('Argument for User.remove() cannot be empty');
+      }
+      const result = await this.collection.deleteOne(selector);
+      return { count: result.deletedCount };
+    } catch (e) {
+      return { error: e };
     }
   }
 }
--- backend/dbtestnew.js
+++ backend/dbtestnew.js
@@ -1,27 +1,35 @@
 const Datastore = require("./datastore");
 
-async function main() {
+async function user() {
   await Datastore.init();
 
-  let result = await Datastore.user.add(    
-    {
-      tag: "NewUser#2", 
-      email: "second@email.com", 
-      discordTag: "second"
-    },
-    {
-      tag: "NewUser#3", 
-      email: "third@email.com", 
-      discordTag: "none"
-    },
-    {
-      tag: "NewUser#4", 
-      email: "fourth@email.com", 
-      discordTag: "fourth"
-    }
-  );
+  // let result = await Datastore.user.add(
+  //   {
+  //     tag: "NewUser#2", 
+  //     email: "second@email.com", 
+  //     discordTag: "second"
+  //   },
+  //   {
+  //     tag: "NewUser#3", 
+  //     email: "third@email.com", 
+  //     discordTag: "none"
+  //   },
+  //   {
+  //     tag: "NewUser#4", 
+  //     email: "fourth@email.com", 
+  //     discordTag: "fourth"
+  //   }
+  // );
+
+  // let result = await Datastore.user.update({tag: "Testuser"}, {discordTag: `testuser#${Math.round(Math.random() * 10000)}`});
+
+  // let result = await Datastore.user.find({tag: "Testuser"});
+
+  // let result = await Datastore.user.remove({tag: "NewUser#2"});
+
+
 
   console.log(result);
 }
 
-main();
+user();
