3de4601317b1b654c77d2b34a70ea6ff38b4bb56
Slav Lazurenko <vlazurenko22@gmail.com>
2021-10-31 03:05:42 -0400

    Created documentation for Datastore and UserDao

--- backend/datastore/index.js
+++ backend/datastore/index.js
@@ -2,18 +2,39 @@ require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const UserDao = require("./UserDAO");
 
+/**
+ * Singleton of datastore used to communicate with MongoDB
+ * @public
+ */
 class Datastore {
+  /**
+   * Initializes Datastore object with MongoClient connection
+   */
   constructor() {
     const url = process.env.MONGODB_URI;
+    /**
+     * MongoDB client reference 
+     * @type {MongoClient}
+     * @private
+     */
     this.client = new MongoClient(url);
   }
 
+  /**
+   * Performs connection to MongoDB and 
+   * initializes database access objects (DAO)
+   */
   async init() {
     try {
       await this.client.connect();
       console.log('Successfully connected to MongoDB');
 
       const db = this.client.db("tms");
+      /**
+       * Database access object (DAO) for users collection
+       * @type {UserDao}
+       * @public
+       */
       this.user = new UserDao(db);
 
     } catch (e) {
