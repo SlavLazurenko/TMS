d02b8a583b359edd22b6f999698de325662f7382
Slav Lazurenko <vlazurenko22@gmail.com>
2021-11-24 21:11:19 -0500

    Profile page HTML and CSS

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,10 +1,57 @@
 import React from "react";
+import "../css/Profile.css";
 
 function Profile() {
 
   return (
     <div className="profile">
-        <h3>Profile</h3>
+      <div className="info-block account">
+        <h2 className="block-name">Account Information</h2>
+        <div className="block">
+          <div className="main">
+            <img className="avatar" src="/img/avatar/default-user-red.png" alt="default user avatar" />
+            <h3 className="username">Slav</h3>
+          </div>
+          <table className="info">
+            <tr>
+              <td className="field">Email:</td>
+              <td className="value">vlazurenko22@gmail.com</td>
+            </tr>
+            <tr>
+              <td className="field">Discord tag:</td>
+              <td className="value">I_SlavKing_I#7837</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+      <div className="info-block team">
+        <h2 className="block-name">Teams</h2>
+        <div className="block">
+          <ul className="team-list">
+            <li className="team-entry">
+              <img className="logo" src="/img/team-logo/default-team.png" alt="default team logo" />
+              <div className="info">
+                <span className="name">Power Rangers</span>
+                <span className="role">Owner</span>
+              </div>
+            </li>
+            <li className="team-entry">
+              <img className="logo" src="/img/team-logo/default-team.png" alt="default team logo" />
+              <div className="info">
+                <span className="name">Power Rangers</span>
+                <span className="role">Owner</span>
+              </div>
+            </li>
+            <li className="team-entry">
+              <img className="logo" src="/img/team-logo/default-team.png" alt="default team logo" />
+              <div className="info">
+                <span className="name">Power Rangers</span>
+                <span className="role">Owner</span>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
