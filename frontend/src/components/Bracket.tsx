a1feb967b7508c1ad58c0ba477f475f76380fba5
Slav Lazurenko <vlazurenko22@gmail.com>
2021-12-03 17:49:34 -0500

    corrected score display

--- frontend/src/components/Bracket.tsx
+++ frontend/src/components/Bracket.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { Bracket, Seed, SeedItem, SeedTeam, SeedTime, RenderSeedProps } from 'react-brackets';
 
 const RenderSeed = ({ breakpoint, seed }: RenderSeedProps) => {
+  const score1 = seed.teams?.[0]?.score;
+  const score2 = seed.teams?.[1]?.score;
+
+  console.log(score1, score2);
+
   return (
     <Seed mobileBreakpoint={breakpoint}>
       <SeedItem style={{ width: '100%' }}>
         <div>
-          <SeedTeam><span>{seed.teams?.[0].name || '-----------'}</span><span>{seed.teams?.[0]?.score || '-'}</span></SeedTeam>
+          <SeedTeam><span>{seed.teams?.[0].name || '-----------'}</span><span>{isNumber(score1) ? score1 : '-'}</span></SeedTeam>
           {/* <div style={{ height: 1, backgroundColor: '#707070' }}></div> */}
-          <SeedTeam><span>{seed.teams?.[1]?.name || '-----------'}</span><span>{seed.teams?.[1]?.score || '-'}</span></SeedTeam>
+          <SeedTeam><span>{seed.teams?.[1]?.name || '-----------'}</span><span>{isNumber(score2) ? score2 : '-'}</span></SeedTeam>
         </div>
       </SeedItem>
       <SeedTime mobileBreakpoint={breakpoint} style={{ fontSize: 9 }}>
@@ -28,5 +33,14 @@ const SingleElimination = (props: any) => {
     />
   );
 };
+
+function isNumber(value: string | number): boolean
+{
+  return (
+    (value != null) &&
+    (value !== '') &&
+    !isNaN(Number(value.toString()))
+  );
+}
 
 export default SingleElimination;
